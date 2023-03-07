96cf7b075d9a1fac0000d6d6f4a0113bae9f1398
Sean Aldrich E. Arceo <searceo@student.apc.edu.ph>
2023-03-07 15:37:32 +0800

    News CSS Addition Test 3

--- ci4/app/Views/news/view.php
+++ ci4/app/Views/news/view.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" type="text/css" href="guests_style.css" />
+<link rel="stylesheet" type="text/css" href="news_style.css" />
 <h2><?= esc($news['title']) ?></h2>
 <p><?= esc($news['body']) ?></p>
