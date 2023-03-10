ab8ff8283cb70d28314de5769b3f9c3b190cee42
Sean Aldrich E. Arceo <searceo@student.apc.edu.ph>
2023-03-10 15:33:10 +0800

    Website: Final CSS Addition

--- ci4/app/Views/pages/success.php
+++ ci4/app/Views/pages/success.php
@@ -1,1 +1,3 @@
+<link rel="stylesheet" type="text/css" href="guest_style.css" /> 
+<link rel="icon" href="image/profile_picture_guest.png" />
 <p>Guest input accepted.</p>
--- ci4/app/Views/pages/view.php
+++ ci4/app/Views/pages/view.php
@@ -1,2 +1,4 @@
+<link rel="stylesheet" type="text/css" href="guest_style.css" /> 
+<link rel="icon" href="image/profile_picture_guest.png" />
 <h2><?= esc($news['name']) ?></h2>
 <p><?= esc($news['comment']) ?></p>
